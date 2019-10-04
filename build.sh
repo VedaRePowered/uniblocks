2c146586b0a97a068e53fe2cad94b20344b85a6c
BEN1JEN <benpheard@gmail.com>
2019-10-04 12:41:11 -0600

    Improve build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 npm i # Make sure deps are up to date
 for file in `ls server/*.js web/*.html web/*.js web/*.css`; do
 	mkdir -p build/`dirname $file`
+	rm build/$file
 	touch build/$file
 	node node_modules/minify/bin/minify.js $file > build/$file
 done
@@ -11,12 +12,26 @@ cp web/*.ico build/web/
 #cp web/*.md build/web/
 
 combiner="
-	--TODO: combine files into html
+	local file = io.open(\"build/web/index.html\")
+	local data = file:read(\"*all\")
+	file:close()
+	file = io.open(\"build/web/index.html\", \"w\")
+	data = data:gsub(\"<script src=([^\\\"=<>]-)>[ \t\n]*</script>\", function(src)
+		local script = io.open(\"build/web\" .. src)
+		if script then
+			local code = script:read(\"*all\")
+			script:close()
+			return \"<script>\" .. code .. \"</script>\"
+		else
+			print(\"Not found: build/web\" .. src)
+		end
+	end)
+	file:write(data)
+	file:close()
 "
 
-for file in `ls build/web/*.html`; do
-	echo $combiner | lua5.3 - $file
-done
+echo $combiner
+echo $combiner | lua5.3 -
 
 # Make the github pages site
 cp README.md docs/info.md
