b9206914b62402a4b59123b1aedbe48a04e23bd3
BEN1JEN <benpheard@gmail.com>
2019-10-15 13:40:11 -0600

    Add basic tile creation to the server.

--- server/main.js
+++ server/main.js
@@ -43,13 +43,21 @@ io.on("connection", function(client) {
 	console.log("Recieved socket.io connection. ID=" + String(playerId));
 	client.on("WorldGetTile", (tileId, sendResp) => {
 		console.log("gettile: " + String(tileId));
-		fs.readFile("./world/tile/" + String(tileId) + ".png", (err, data) => {
+		fs.readFile("./world/tile/" + String(tileId) + ".png", (err, pngData) => {
 			if (err) {
 				console.log(err);
 				sendResp(false, err);
 			} else {
-				console.log("s");
-				sendResp(true, {"name": "TID=" + String(tileId), "graphic": "data:image/png;base64," + data.toString("base64"), "code": ""});
+				fs.readFile("./world/tile/" + String(tileId) + ".json", (err, jsonData) => {
+					if (err) {
+						console.log(err);
+						sendResp(false, err);
+					} else {
+						const info = JSON.parse(jsonData);
+						console.log("s");
+						sendResp(true, {"name": info.name, "description": info.description, "graphic": "data:image/png;base64," + pngData.toString("base64"), "code": info.code});
+					}
+				})
 			}
 		});
 	});
@@ -59,9 +67,29 @@ io.on("connection", function(client) {
 			sendResp(getRegion(x*256, y*256));
 		}
 	});
-	client.on("WorldNewTile", (graphic, name, code, sendResp) => {
-		const newTileId = tiles.length;
-		tiles[newTileId] = new Tile(graphic, name, code);
+	client.on("WorldNewTile", (graphic, name, description, code, sendResp) => {
+		const newTileId = ruid();
+		fs.writeFile("./world/tile/" + String(newTileId) + ".json", JSON.stringify({"name": name, "description": description, "code": code}));
+		const img = new PNG({
+			"width": 16,
+			"height": 16,
+			"filterType": -1
+		});
+		for (let x = 0; x < 16; x++) {
+			for (let y = 0; y < 16; y++) {
+				const r = (graphic.palette[graphic.grid[x+y*16]] & 0xff000000) >>> 24;
+				const g = (graphic.palette[graphic.grid[x+y*16]] & 0x00ff0000) >>> 16;
+				const b = (graphic.palette[graphic.grid[x+y*16]] & 0x0000ff00) >>> 8;
+				const a = graphic.palette[graphic.grid[x+y*16]] & 0x000000ff;
+				console.log(r, g, b, a)
+				img.data[x*4+y*64] = r;
+				img.data[x*4+y*64+1] = g;
+				img.data[x*4+y*64+2] = b;
+				img.data[x*4+y*64+3] = a;
+			}
+		}
+		img.pack().pipe(fs.createWriteStream("./world/tile/" + String(newTileId) + ".png"));
+
 		if (typeof(sendResp) === "function") {
 			sendResp(newTileId);
 		}
